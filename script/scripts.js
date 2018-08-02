3123b304b6500f0649b789240732cdcff813422a
Robert Eklund <robert.eklund3@gmail.com>
2018-08-01 20:15:12 -0500

    Update with API Linking

--- script/scripts.js
+++ script/scripts.js
@@ -1,23 +1,24 @@
 "use strict";
-
+var coinPrice;
 window.onload = function()
 {
-    var request = new XMLHttpRequest();
-    request.open('GET', 'https://api.pro.coinbase.com', true);
-    
-    request.onload = function ()
-    {
-        var data = JSON.parse(this.response);
-        if (request.status >= 200 && request.status < 400)
-        {
-            console.log('Connection Success!');
-        }
-        else
-        {
-            console.log('ERROR!!');
-        }
-        request.send();
-    console.log(request.status);
 
-    }
+    getPrice();
+    console.log(coinPrice);
 }
+
+function getPrice()
+{
+    var jqxhr = $.getJSON( "https://api.coinbase.com/v2/prices/ETH-USD/spot", {},function(data) {
+        $("#coinPrice").text(data.data.amount);
+        coinPrice = JSON.stringify(data.data.amount);
+    })
+}
+/*
+ask the user to enter a list of students for a list of students -- put the students in a stack
+prompt to enter the grades for the students for each student add a list of grades -- dictionary of student grades[];
+program should output the avg grade for each student
+
+req: implement a student to grade array dictionary
+for practice you could implement the student input as a stack
+*/

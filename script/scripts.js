c218f6126d0b657647e7e0e39b0b36d81b92fd27
Robert Eklund <robert.eklund3@gmail.com>
2018-08-20 18:18:11 -0500

    update to function and transaction array

--- script/scripts.js
+++ script/scripts.js
@@ -1,6 +1,6 @@
 "use strict";
 var coinPrice;
-var transaction = [[]];
+var transaction = [];
 window.onload = function()
 {
     getPrice();
@@ -12,21 +12,15 @@ function getPrice()
     var jqxhr = $.getJSON( "https://api.coinbase.com/v2/prices/ETH-USD/spot", {},function(data) {
         $("#coinPrice").text("$ " + data.data.amount);
         coinPrice = data.data.amount;
-        // coinPrice = JSON.stringify(data.data.amount);
-        //console.log(data.data.amount);
     })
 }
 
-$("#trade-submit-button").on('click', function ()
-{
-    transaction[0] = $('#trans-date').val();
-    transaction[1] = $('#quantity-input').val();
-    transaction[2] = $('#trans-price-input').val();
-    transaction[3] = $('#sell-buy-select').val();
-    transaction.forEach(function(){
-        console.log(transaction[0]);
-        console.log(transaction[1]);
-        console.log(transaction[2]);
-        console.log(transaction[3]);
+$("#trade-submit-button").on('click', () => {
+    transaction.push({
+        transactionDate: $('#trans-date').val(),
+        quantity: $('#quantity-input').val(),
+        price: $('#trans-price-input').val(),
+        transactionType: $('#sell-buy-select').val(),    
     });
+    console.log(transaction);
 });
